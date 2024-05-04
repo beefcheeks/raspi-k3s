36cfb2464a89f8e904b3b870f404b1ffb4d4b9a6
Ryan Staatz <rcstaatz@gmail.com>
2024-05-04 14:53:29 -0700

    fix: use correct blueprint names
    - existing automations expect this
    - also update script to compare base file names

--- argocd/apps/home-assistant/base/files/blueprints-manager.sh
+++ argocd/apps/home-assistant/base/files/blueprints-manager.sh
@@ -5,20 +5,27 @@ MONITORED_DIR="/opt/blueprints/"
 TARGET_DIR="/config/blueprints/automation/custom/"
 SLEEP_SECONDS=30
 
+# Function to calculate MD5 checksums of files in a directory
+calculate_checksums() {
+    # Calculate MD5 checksums and base file names in the specified directory
+    find "$1" -type f -exec md5sum {} + | awk -F / '{print $1 $NF}' | sort
+}
+
 # Function to monitor changes
 monitor_changes() {
     while true; do
         # Calculate MD5 checksums of files in the monitored directory
-        monitored_checksums=$(find "$MONITORED_DIR" -type f -exec md5sum {} + | awk '{print $1}' | sort | md5sum | awk '{print $1}')
+        monitored_checksums=$(calculate_checksums "$MONITORED_DIR")
 
         # Calculate MD5 checksums of files in the target directory
-        target_checksums=$(find "$TARGET_DIR" -type f -exec md5sum {} + | awk '{print $1}' | sort | md5sum | awk '{print $1}')
+        target_checksums=$(calculate_checksums "$TARGET_DIR")
 
         # Compare the checksums of the two directories
         if [ "$monitored_checksums" != "$target_checksums" ]; then
             # Perform actions when differences are detected
             echo "Differences detected between $MONITORED_DIR and $TARGET_DIR"
-            echo "Copying files from $MONITORED_DIR to $TARGET_DIR..."
+            echo "Deleting then copying files from $MONITORED_DIR to $TARGET_DIR..."
+            rm "$TARGET_DIR"/*
             cp -rL "$MONITORED_DIR"/* "$TARGET_DIR"
             echo "Files copied successfully."
         fi
